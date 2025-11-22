324e108b17589353a837c009feea749e9f4dc6db
paulohenrique64 <paulohenriquelvs20@gmail.com>
2025-11-22 19:40:20 -0300

    feat: integrate Traefik and configure routing for frontend, gateway and wall microservices

--- gateway/config.py
+++ gateway/config.py
@@ -8,6 +8,8 @@ class Config:
     
     API_GATEWAY_HOST = os.getenv('API_GATEWAY_HOST', '0.0.0.0')
     API_GATEWAY_PORT = int(os.getenv('API_GATEWAY_PORT', 5002))
+
+    WSGI_API_PREFIX = os.getenv('WSGI_API_PREFIX', '/api')
     
     AUTH_API_URL = os.getenv('AUTH_API_URL', 'http://localhost:3333')
     BLOCKCHAIN_API_URL = os.getenv('BLOCKCHAIN_API_URL', 'http://localhost:5001')
